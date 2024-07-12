254964b7ac3985401c429b6ab090cfca0a13e8da
Nefta Hernandez <neftaligow01@gmail.com>
2024-07-11 18:51:10 -0600

    feat: Update ListLinks component to display the name of the authenticated user

--- src/components/ListLinks.tsx
+++ src/components/ListLinks.tsx
@@ -1,9 +1,15 @@
+import { authOptions } from "@/lib/auth"
 import DisplayLinks from "./DisplayLinks"
+import { getServerSession } from "next-auth"
 
-const ListLinks = () => {
+const ListLinks = async () => {
+  
+  const session = await getServerSession(authOptions)
+  const name = session?.user.username
+  
   return (
     <div className="flex flex-col justify-center">
-        <h3 className="text-xl font-semibold text-center">All your links:</h3>
+        <h3 className="text-xl font-semibold text-center">All links of {name}:</h3>
         <DisplayLinks />
     </div>
   )
